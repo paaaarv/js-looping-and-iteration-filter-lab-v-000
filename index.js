3ff5de28ab954d98e43a38abe19e60dad800203a
Parv Kumar <parvathikumar92@gmail.com>
2017-10-19 18:43:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 function findMatching(drivers, string){
-  const matchingDrivers = drivers.filter(function(driver){ debugger 
-    return driver.toLowerCase().indexOf(string.toLowerCase()) > -1;
+  const matchingDrivers = drivers.filter(function(driver){ 
+    return driver === string
   })
 }
