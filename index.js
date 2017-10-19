8c8886b858f6c31b34c3b2db36c9ce866e312ee2
Parv Kumar <parvathikumar92@gmail.com>
2017-10-19 18:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 function findMatching(drivers, string){
   let matchingDrivers = drivers.filter(function(driver){
-    return driver.toLowerCase().indexOf(string.toLowerCase()) > -1;
+    return driver.toLowerCase().indexOf(string.toLowerCase())) > -1;
 }
 }
